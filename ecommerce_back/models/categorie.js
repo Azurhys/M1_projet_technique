541e7a361eca8020007022ab5e81c3d4307861fd
Azurhys <123641752+Azurhys@users.noreply.github.com>
2024-07-15 16:32:16 +0200

    endpoints catégories + fichier inser categories

---
+++ ecommerce_back/models/categorie.js
@@ -0,0 +1,10 @@
+class Category {
+    constructor(id_categorie, nom, id_categorie_parent) {
+      this.id_categorie = id_categorie;
+      this.nom = nom;
+      this.id_categorie_parent = id_categorie_parent;
+    }
+  }
+  
+  module.exports = Category;
+  
