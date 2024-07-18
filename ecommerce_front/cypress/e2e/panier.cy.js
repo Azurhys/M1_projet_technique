53ea45bc22a9c5340c772f71d02e914e6e385d49
azurhys <azurhys27@gmail.com>
2024-07-18 16:45:15 +0200

    test pipe headed

--- ecommerce_front/cypress/e2e/panier.cy.js
+++ ecommerce_front/cypress/e2e/panier.cy.js
@@ -1,33 +1,35 @@
 describe('Scénario ajout au panier + commande', () => {
     beforeEach(() => {
-        cy.visit('/')
+      cy.visit('/')
     });
   
     it('Commande un article', () => {
-        cy.intercept('POST', '**/auth/login').as('loginRequest');    
-        cy.get(':nth-child(2) > .nav-link').click({force: true});
-        cy.url().should('eq', 'http://localhost:5173/articles');
-        cy.get('.container > :nth-child(2) > :nth-child(1)').click();
-        cy.get('.btn').click();
-        cy.wait(3000);
-        cy.get(':nth-child(4) > .nav-link').click({force: true});
-        cy.url().should('eq', 'http://localhost:5173/panier');
-        cy.get('.list-group-item')
-        cy.get('.mt-5 > .btn').click({force: true});
-        cy.get('input[type="email"]').type('admin@example.com');
-        cy.get('input[type="password"]').type('admin123');
-        cy.get('button[type="submit"]').click();
-        cy.wait('@loginRequest').then((interception) => {
-            expect(interception.response.statusCode).to.equal(200);
-            cy.url().should('eq', 'http://localhost:5173/');
-          });
-        cy.get(':nth-child(5) > .nav-link').click({force: true})
-        cy.url().should('eq', 'http://localhost:5173/panier')
-        cy.get('.mt-5 > .btn').click()
-        cy.url().should('eq', 'http://localhost:5173/recap-panier')
-        //cy.get('.mt-5 > .btn').click()
+      cy.intercept('POST', '**/auth/login').as('loginRequest');    
+      cy.get(':nth-child(2) > .nav-link').click({force: true});
+      cy.url().should('eq', 'http://localhost:5173/articles');
+      cy.get('.container > :nth-child(2) > :nth-child(1)').click();
+      cy.get('.btn').click();
+      cy.wait(3000);
+      cy.get(':nth-child(4) > .nav-link').click({force: true});
+      cy.url().should('eq', 'http://localhost:5173/panier');
+      cy.get('.list-group-item')
+      cy.get('.mt-5 > .btn').click({force: true});
+      cy.get('input[type="email"]').type('admin@example.com');
+      cy.get('input[type="password"]').type('admin123');
+      cy.get('button[type="submit"]').click();
+  
+      cy.wait('@loginRequest').then((interception) => {
+        expect(interception.response.statusCode).to.equal(200);
+      });
+  
+      cy.wait(2000); // Ajout d'un délai d'attente supplémentaire
+  
+      cy.url().should('eq', 'http://localhost:5173/');
+      cy.get(':nth-child(5) > .nav-link').click({force: true})
+      cy.url().should('eq', 'http://localhost:5173/panier')
+      cy.get('.mt-5 > .btn').click()
+      cy.url().should('eq', 'http://localhost:5173/recap-panier')
+      //cy.get('.mt-5 > .btn').click()
     });
-    
-    
   });
   
